b2252acc35e076923c0ebe5a87703b3de828312d
Naehee Kim <tyche92@gmail.com>
2013-04-15 11:12:14 -0700

    Adding new MyRecommendationView

--- StreamTest/MyRecommendationViewController.h
+++ StreamTest/MyRecommendationViewController.h
@@ -2,12 +2,28 @@
 //  MyRecommendationViewController.h
 //  StreamTest
 //
-//  Created by Naehee Kim on 4/13/13.
+//  Created by Vimal Kini on 4/9/13.
 //
 //
 
 #import <UIKit/UIKit.h>
+#import "UserStore.h"
+#import "UserData.h"
 
-@interface MyRecommendationViewController : UIViewController
+@class Rec;
+@class Comment;
+
+@interface MyRecommendationViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic) int originalHeight;
+@property (nonatomic, strong) Rec *rec;
+@property (nonatomic, strong) NSMutableArray *comments;
+@property (nonatomic, strong) NSMutableData *responseData;
+@property (nonatomic, strong) UIScrollView *scrollView;
+@property (nonatomic, strong) UIImage *detailPicImage;
+@property (nonatomic, strong) UITableView *commentTableView;
+@property (nonatomic, strong) NSString *productNameAndPurchasePlace;
+@property (nonatomic, strong) UserData *myUserdata;
+- (void)renderTable;
 
 @end
--- StreamTest/MyRecommendationViewController.m
+++ StreamTest/MyRecommendationViewController.m
@@ -2,17 +2,27 @@
 //  MyRecommendationViewController.m
 //  StreamTest
 //
-//  Created by Naehee Kim on 4/13/13.
+//  Created by Vimal Kini on 4/9/13.
 //
 //
 
 #import "MyRecommendationViewController.h"
+#import <SDWebImage/UIImageView+WebCache.h>
+#import "DataFeedStore.h"
+#import "Rec.h"
+#import "Comment.h"
+#import "UserStore.h"
+#import "UserData.h"
+
 
 @interface MyRecommendationViewController ()
 
 @end
 
 @implementation MyRecommendationViewController
+
+@synthesize originalHeight, rec, productNameAndPurchasePlace;
+@synthesize scrollView, detailPicImage, commentTableView, myUserdata;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -26,7 +36,44 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    UserStore *userstore = [UserStore sharedStore];
+    self.myUserdata = userstore.userData;
+    
     // Do any additional setup after loading the view from its nib.
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    NSLog(@"You have %d friends on toddle ", [myUserdata.toddleFriends count]);
+    return [myUserdata.toddleFriends count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    static NSString *CellIdentifier = @"MainCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    if (cell == nil){
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    //get friends data
+    NSUInteger row = [indexPath row];
+    cell.textLabel.text = [[myUserdata.toddleFriends objectAtIndex:row] objectForKey:@"friendname"];
+    
+    NSString *friendImageUrl = [[myUserdata.toddleFriends objectAtIndex:row] objectForKey:@"friendurl"];
+    NSURL *imageURL = [NSURL URLWithString:friendImageUrl];
+    NSData *imageData = [NSData dataWithContentsOfURL:imageURL];
+    UIImage *image = [UIImage imageWithData:imageData];
+    cell.imageView.image = image;
+    
+    return cell;
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
