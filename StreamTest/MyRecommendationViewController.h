ef48b9cf3f14358832bc5c7e7c0b1baafb321135
Naehee Kim <tyche92@gmail.com>
2013-04-15 11:11:31 -0700

    commit - after deleting MyRecommendation... and adding them

---
+++ StreamTest/MyRecommendationViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyRecommendationViewController.h
+//  StreamTest
+//
+//  Created by Naehee Kim on 4/13/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyRecommendationViewController : UIViewController
+
+@end
---
+++ StreamTest/MyRecommendationViewController.m
@@ -0,0 +1,38 @@
+//
+//  MyRecommendationViewController.m
+//  StreamTest
+//
+//  Created by Naehee Kim on 4/13/13.
+//
+//
+
+#import "MyRecommendationViewController.h"
+
+@interface MyRecommendationViewController ()
+
+@end
+
+@implementation MyRecommendationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
